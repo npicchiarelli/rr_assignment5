c08dfb189d33b7895a20a450fbee75d70c7c9342
npicchiarelli <picchiarelliniccolo@gmail.com>
2026-02-03 12:20:47 +0100

    add statistical analysis

--- analysis_code/analysis_code.jl
+++ analysis_code/analysis_code.jl
@@ -14,8 +14,6 @@ end
 metadata = JSON.parsefile(joinpath(raw_data_dir, "metadata.json"))
 scale_factor = metadata["scale-factor-px-micron"]
 
-df = DataFrame() # Initialize empty DataFrame
-
 for file in readdir(raw_data_dir)
     println("Found file: $file")
     if endswith(file, ".csv") # Filter for CSV files
@@ -28,3 +26,16 @@ for file in readdir(raw_data_dir)
         CSV.write(joinpath(processed_data_dir, processed_data_fname), data[:,[:Area_px, :Diameter_μm]]) # Save only relevant columns
     end
 end
+
+df = DataFrame() # Initialize empty DataFrame
+for file in readdir(processed_data_dir)
+    if endswith(file, "_processed.csv")
+        temp_df = CSV.read(joinpath(processed_data_dir, file), DataFrame)
+        file_name = replace(file, "_processed.csv" => "")
+        min_diameter = minimum(temp_df.Diameter_μm)
+        max_diameter = maximum(temp_df.Diameter_μm)
+        median_diameter = median(temp_df.Diameter_μm)
+        push!(df, (File=file_name, Min_Diameter_μm=min_diameter, Max_Diameter_μm=max_diameter, Median_Diameter_μm=median_diameter))
+    end
+end
+println(df)
