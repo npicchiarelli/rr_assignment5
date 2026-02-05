426bc0d6c3b5dc14a9d9a5855f65193a66c7be54
npicchiarelli <picchiarelliniccolo@gmail.com>
2026-02-05 11:10:18 +0100

    Add docstrings for area2diam and px2micron

--- analysis_code/utilities.jl
+++ analysis_code/utilities.jl
@@ -4,6 +4,14 @@ Computes the diameter of a circle with given area.
 
 # Examples
 ```julia-repl
+julia> area = 100
+100
+
+julia> d = area2diam(area)
+11.283791670955125
+
+julia> π*(d/2)^2
+99.99999999999999
 ```
 """
 function area2diam(area)
@@ -16,6 +24,8 @@ Computes the length in microns given a length in pixels and a scale factor.
 
 # Examples
 ```julia-repl
+julia> px2micron(100, 2.5)
+40.0
 ```
 """
 function px2micron(length, scale_factor)
